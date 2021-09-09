2c93812598244796178ffe3d1ffdf3b8359804ef
Matt Oldenburg <theoldenmw@gmail.com>
2021-09-08 21:16:47 -0400

    finalized error handling

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,29 +27,23 @@ var getWeatherInfo = function(lat, lon, cityName) {
 
             storeButtons();
         });
-    }).catch(function(error) {
-        currentWeatherEl.innerHTML = "";
-        currentWeatherEl.setAttribute("class", "current-weather border border-secondary");
-        
-        var headerEl = document.createElement("h3");
-        headerEl.innerText = "There was an error finding the weather!";
-        headerEl.setAttribute("class", "fs-3");
-        currentWeatherEl.appendChild(headerEl);
     });
 };
 var getCityCoords = function(cityName) {
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=" + apiKey).then(function(response) {
-        response.json().then(function(data) {
-            getWeatherInfo(data.coord.lat, data.coord.lon, cityName)
-        });
-    })catch(function(error) {
-        currentWeatherEl.innerHTML = "";
-        currentWeatherEl.setAttribute("class", "current-weather border border-secondary");
+        if (response.ok) {
+            response.json().then(function(data) {
+                getWeatherInfo(data.coord.lat, data.coord.lon, cityName)
+            });
+        } else {
+            currentWeatherEl.innerHTML = "";
+            currentWeatherEl.setAttribute("class", "current-weather border border-secondary");
         
-        var headerEl = document.createElement("h3");
-        headerEl.innerText = "There was an error finding the weather!";
-        headerEl.setAttribute("class", "fs-3");
-        currentWeatherEl.appendChild(headerEl);
+            var headerEl = document.createElement("h3");
+            headerEl.innerText = "There was an error finding the weather!";
+            headerEl.setAttribute("class", "fs-3");
+            currentWeatherEl.appendChild(headerEl);
+        }
     });
 }
 
